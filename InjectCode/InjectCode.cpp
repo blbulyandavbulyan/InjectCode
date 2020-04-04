e71b372b89f465cd7efd0d57c1bc16b268707284
Давид Блбулян <blbulyandavbulyan@gmail.com>
2020-04-04 22:21:44 +0300

    Добавьте файлы проекта.

---
+++ InjectCode/InjectCode.cpp
@@ -0,0 +1,18 @@
+﻿// InjectCode.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "stdafx.h"
+typedef std::wstring WSTRING;
+typedef std::string STRING;
+#ifdef _UNICODE
+typedef WSTRING TSTRING;
+#else
+typedef STRING TSTRING;
+#endif
+struct ProgrammParameters {
+    TSTRING PathToInjectFile;
+    TSTRING PathToInjectedFile;
+};
+int _tmain(int argc, TCHAR *argv[]){
+    
+    return 0;
+}
---
+++ InjectCode/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+//файлы заголовков Windows
+#include <windows.h>
+#include <tchar.h>
+//Файлы заголовков C++
+#include <iostream>
+#include <string.h>
